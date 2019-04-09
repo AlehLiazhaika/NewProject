2d92a7890f3a293ccbc624a1ecfbb86ec578069c
AlehLiazhaika <aleh.liazhaika@gmail.com>
2019-04-09 10:53:42 +0300

    fixfixfix

--- mainTask/scripts/MainPage/mainPage.js
+++ mainTask/scripts/MainPage/mainPage.js
@@ -1,5 +1,8 @@
 /* global PhotoPost, Feed, Header */
 
+if (!localStorage.getItem('posts')) {
+  localStorage.setItem('posts', JSON.stringify([]));
+}
 
 const header = new Header();
 const feed = new Feed(document.getElementById('feed'));
